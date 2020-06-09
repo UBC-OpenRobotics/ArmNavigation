420e4397177dacf8fb57869800c4fef1cf4dffd4
Andrew <andrewxieax1@gmail.com>
2020-06-09 14:21:28 -0700

    voice command launch files and pick up command

--- arm_nav/src/voice_command_arm_nav.py
+++ arm_nav/src/voice_command_arm_nav.py
@@ -8,33 +8,50 @@ from geometry_msgs.msg import PoseStamped, Pose
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 def wave():
-
     # Start the arm in the "resting" pose stored in the SRDF file
     arm.set_named_target('wave_1')
     arm.go()
-    # Start the arm in the "resting" pose stored in the SRDF file
     arm.set_named_target('wave_2')
     arm.go()
 
+def resting():
+    # Start the arm in the "resting" pose stored in the SRDF file
+    arm.set_named_target('resting')
+    arm.go()
+
+def doAction(voice_command):
+    command = voice_command.data.lower()
+
+    if command in {'wave', 'goodbye'}:
+        wave()
+    elif command in {"stop"}:
+        resting()
+    elif command in {"pick up"}:
+        x=0
+        y=0.2
+        z=0.45
+        pub = rospy.Publisher('arm_coordinate', Point, queue_size=10)
+        pub.publish(float(x),float(y),float(z))
+
+def listener()
+    rospy.Subscriber("grammar_data", String, doAction)
+    rospy.spin()
+
 if __name__ == '__main__':
 
-    try:
-        # Initialize the move_group API
-        moveit_commander.roscpp_initialize(sys.argv)
-            
-        rospy.init_node('arm_voice_command_listener')
-                    
-        # Initialize the move group for the right arm
-        arm = moveit_commander.MoveGroupCommander('arm')
-
-        # Allow replanning to increase the odds of a solution
-        arm.allow_replanning(True)
-
-            # Allow some leeway in position (meters) and orientation (radians)
-        arm.set_goal_position_tolerance(0.001)
-        arm.set_goal_orientation_tolerance(0.005)
+    # Initialize the move_group API
+    moveit_commander.roscpp_initialize(sys.argv)
         
-        while not rospy.is_shutdown():
-            wave()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('arm_voice_command')
+                
+    # Initialize the move group for the right arm
+    arm = moveit_commander.MoveGroupCommander('arm')
+
+    # Allow replanning to increase the odds of a solution
+    arm.allow_replanning(True)
+
+        # Allow some leeway in position (meters) and orientation (radians)
+    arm.set_goal_position_tolerance(0.001)
+    arm.set_goal_orientation_tolerance(0.005)
+        
+    listener()
